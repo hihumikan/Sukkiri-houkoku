515407ada109c7b2deb4f8c94093e1301f435cf8
hihumikan <musecharo@gmail.com>
2021-09-13 22:53:31 +0900

    react-headの追加

--- src/index.js
+++ src/index.js
@@ -2,10 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="Twitter:site" content="@mikan_54951" />
+        <meta name="twitter:title" content="スッキリ報告" />
+        <meta name="twitter:description" content="はーーすっきりした" />
+        <meta
+          name="twitter:image"
+          content="https://2.bp.blogspot.com/-PZ6b5vGDuAE/VYJjWRTCK5I/AAAAAAAAuas/3MWHtJZTLXc/s400/toilet_benza_shimeru_girl.png"
+        />
+        <title>スッキリ報告</title>
+      </Helmet>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
